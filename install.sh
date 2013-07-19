0180dc7e2e2489cda19ee17404f75406dc44be8a
Dain Nilsson <dain@yubico.com>
2013-07-19 17:43:29 +0200

    Added installation script.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+git submodule update --init
+echo "Submodules updated."
+
+VIMRC="../.vimrc"
+
+if [ ! -h $VIMRC ] ; then
+	if [ -f $VIMRC ] ; then
+		mv $VIMRC $VIMRC.bak
+		echo "Backup of .vimrc made."
+	fi
+	ln -s .vim/.vimrc $VIMRC
+	echo ".vimrc symlink created."
+fi
